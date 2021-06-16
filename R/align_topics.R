4cd4a306e3af61a6a508d86aa80539ed69a2c962
Kris Sankaran <sankaran.kris@gmail.com>
2021-06-16 15:51:27 -0500

    Setup the S4 class for alignment.

--- R/align_topics.R
+++ R/align_topics.R
@@ -17,10 +17,15 @@ align_topics <- function(
   models,
   comparisons = "consecutive",
   method = "product",
-  order_constrain = NULL
+  order_constrain = NULL,
+  ...
 ) {
   check_input(models, comparisons, method)
   weight_fun <- ifelse(method == "product", product_weights, transport_weights)
+  if (comparisons == "consecutive") {
+    k_ <- length(models)
+    comparisons <- data.frame(from = seq_len(k_ - 1), to = seq(2, k_))
+  }
 
   alignment <- align_graph(
     comparisons,
@@ -32,10 +37,12 @@ align_topics <- function(
   # 3. ORDER TOPICS
 
   # 4. Return results
-  alignment
+  new(
+    "alignment",
+    weights = as.data.frame(alignment)
+  )
 }
 
-models <- get(load("~/Desktop/models.RData"))
 
 #' @importFrom purrr map_int
 check_input <- function(
@@ -58,6 +65,10 @@ check_input <- function(
   match.arg(tolower(method), c("product", "transport"))
 }
 
+#' Alignment between Pairs of Topics
+#' @importFrom dplyr mutate
+#' @importFrom magrittr %>%
+#' @export
 align_graph <- function(edges, gamma_hats, beta_hats, weight_fun, ...) {
   weights <- list()
   for (i in seq_len(nrow(edges))) {
@@ -69,7 +80,7 @@ align_graph <- function(edges, gamma_hats, beta_hats, weight_fun, ...) {
 }
 
 #' @importFrom purrr map
-#' @importFrom dplyr %>%
+#' @importFrom magrittr %>%
 product_weights <- function(gammas, ...) {
   products <- t(gammas[[1]]) %*% gammas[[2]]
   dimnames(products) <- purrr::map(gammas, ~ colnames(.))
@@ -84,10 +95,11 @@ transport_weights <- function(gammas, ...) {
 ################################################################################
 # Helper functions for reshaping and renaming
 ################################################################################
-
-#' @importFrom dplyr %>% mutate
+#' @importFrom dplyr mutate
 #' @importFrom tibble rownames_to_column
 #' @importFrom tidyr pivot_longer
+#' @importFrom magrittr %>%
+#' @importFrom stringr str_replace
 .lengthen_weights <- function(weights) {
   weights %>%
     rownames_to_column("k_LDA") %>%
@@ -95,7 +107,8 @@ transport_weights <- function(gammas, ...) {
     mutate(k_LDA_next = str_replace(k_LDA_next, "X", ""))
 }
 
-#' @importFrom dplyr %>% group_by bind_rows summarize mutate ungroup arrange
+#' @importFrom dplyr group_by bind_rows summarize mutate ungroup arrange across
+#' @importFrom magrittr %>%
 postprocess_weights <- function(weights, n_docs, m_levels) {
   bind_rows(weights) %>%
     group_by(m, m_next, k_LDA, k_LDA_next) %>%
@@ -107,3 +120,66 @@ postprocess_weights <- function(weights, n_docs, m_levels) {
     mutate(across(c("m", "m_next"), factor, levels = m_levels)) %>%
     arrange(m)
 }
+
+################################################################################
+# Class construction and methods
+################################################################################
+print_alignment <- function(x) {
+  cat(
+    "An object of class ", class(x),
+    " comparing ",
+    n_models(x),
+    " models across ",
+    n_topics(x),
+    " topics\n"
+  )
+
+  cat("#################################################################\n")
+  print(head(x@weights))
+  cat("....")
+  cat("#################################################################\n")
+}
+
+plot_alignment <- function(x, y, ...) {
+  warning("plotting method is not implemented yet")
+}
+
+#' Alignment Class Definition
+#' @import methods
+#' @exportClass alignment
+setClass("alignment",
+  representation(
+    weights = "data.frame",
+    n_models = "numeric",
+    n_topics = "numeric"
+  )
+)
+
+#' Plot Method for Alignment Class
+#' @import methods
+#' @export
+setMethod("plot", c(x = "alignment", y = "missing"), plot_alignment)
+
+#' Show Method for Alignment Class
+#' @import methods
+#' @export
+setMethod("show", "alignment", print_alignment)
+
+setGeneric("weights", function(x) standardGeneric("weights"))
+#' Weights Accessor for Alignment Class
+#' @import methods
+#' @export
+setMethod("weights", "alignment", function(x) x@weights)
+
+setGeneric("n_models", function(x) standardGeneric("n_models"))
+
+#' Number of Models Method for Alignment Class
+#' @import methods
+#' @export
+setMethod("n_models", "alignment", function(x) length(unique(x@weights$m, x@weights$m_next)))
+
+setGeneric("n_topics", function(x) standardGeneric("n_topics"))
+#' Number of Topics Method for Alignment Class
+#' @import methods
+#' @export
+setMethod("n_topics", "alignment", function(x) length(unique(x@weights$k_LDA, x@weights$k_LDA_next)))
